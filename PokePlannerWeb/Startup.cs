8ce27d3e97f45a0a353bd1e45e57c14608f23d28
Julian Lawrance <julian.lawrance@gmail.com>
2019-11-09 22:43:25 +0000

    Do CodeMaid formatting

--- PokePlannerWeb/Startup.cs
+++ PokePlannerWeb/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,7 +19,6 @@ namespace PokePlannerWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllersWithViews();
 
             // In production, the React files will be served from this directory
