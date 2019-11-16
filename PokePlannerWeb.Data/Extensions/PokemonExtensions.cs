c56c39cb374579eb35fa8605d7ad3eae2fd793cd
Julian Lawrance <julian.lawrance@gmail.com>
2019-11-16 20:45:30 +0000

    Create payload classes for presenting API resources to React components

---
+++ PokePlannerWeb.Data/Extensions/PokemonExtensions.cs
@@ -0,0 +1,24 @@
+﻿using PokeApiNet.Models;
+using PokePlannerWeb.Data.Payloads;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Type = PokePlannerWeb.Data.Mechanics.Type;
+
+namespace PokePlannerWeb.Data.Extensions
+{
+    /// <summary>
+    /// Extension methods for the PokeAPI Pokemon type.
+    /// </summary>
+    public static class PokemonExtensions
+    {
+        /// <summary>
+        /// Returns a minimal representation of this Pokemon resource.
+        /// </summary>
+        public static async Task<PokemonPayload> AsPayload(this Pokemon pokemon)
+        {
+            return await PokemonPayload.Create(pokemon);
+        }
+    }
+}
