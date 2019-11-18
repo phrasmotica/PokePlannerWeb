b28cda99d33e30b8734d1ff0a26e037f1f7989fe
Julian Lawrance <julian.lawrance@gmail.com>
2019-11-18 21:37:32 +0000

    Remove unnecessary classes, pages and components

--- PokePlannerWeb/ClientApp/src/App.js
+++ PokePlannerWeb/ClientApp/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
 import { TeamBuilder } from './components/TeamBuilder';
-import { Counter } from './components/Counter';
 
 import './custom.css'
 
@@ -13,9 +11,7 @@ export default class App extends Component {
     render() {
         return (
             <Layout>
-                <Route exact path='/' component={Home} />
-                <Route path='/counter' component={Counter} />
-                <Route path='/team-builder' component={TeamBuilder} />
+                <Route exact path='/' component={TeamBuilder} />
             </Layout>
         );
     }
