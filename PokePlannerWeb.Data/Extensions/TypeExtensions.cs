85ad53a8d218f60b0b056cca1ad32d6fd757aab3
Julian Lawrance <julian.lawrance@gmail.com>
2019-11-18 22:06:28 +0000

    Create controller class for PokeAPI type resources

---
+++ PokePlannerWeb.Data/Extensions/TypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using PokeApiNet.Models;
+using PokePlannerWeb.Data.Payloads;
+
+namespace PokePlannerWeb.Data.Extensions
+{
+    /// <summary>
+    /// Extension methods for the PokeAPI Type type.
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// Returns a minimal representation of this Type resource.
+        /// </summary>
+        public static ResourcePayload<Type> AsPayload(this Type type)
+        {
+            return new ResourcePayload<Type>(type);
+        }
+    }
+}
---
+++ PokePlannerWeb/Controllers/TypeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PokeApiNet.Models;
+using PokePlannerWeb.Data;
+using PokePlannerWeb.Data.Extensions;
+using PokePlannerWeb.Data.Payloads;
+using System.Threading.Tasks;
+
+namespace PokePlannerWeb.Controllers
+{
+    /// <summary>
+    /// Controller for calling Type resource endpoints.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class TypeController : ControllerBase
+    {
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        private readonly ILogger<TypeController> Logger;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public TypeController(ILogger<TypeController> logger)
+        {
+            Logger = logger;
+        }
+
+        /// <summary>
+        /// Returns the Type with the given name.
+        /// </summary>
+        [HttpGet("{name}")]
+        public async Task<ResourcePayload<Type>> GetType(string name)
+        {
+            Logger.LogInformation($"Getting type \"{name}\"...");
+            var type = await PokeApiData.Instance.Get<Type>(name);
+            return type.AsPayload();
+        }
+    }
+}
