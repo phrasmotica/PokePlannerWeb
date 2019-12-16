a0958f69df7c4b215406034a617323a54d47a93a
Julian Lawrance <julian.lawrance@gmail.com>
2019-12-16 22:22:13 +0000

    Create controller for getting Pokemon type efficacy

---
+++ PokePlannerWeb/Controllers/EfficacyController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PokeApiNet.Models;
+using PokePlannerWeb.Data;
+using PokePlannerWeb.Data.Extensions;
+using System.Threading.Tasks;
+
+namespace PokePlannerWeb.Controllers
+{
+    /// <summary>
+    /// Controller for getting Pokemon type efficacies.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class EfficacyController : ControllerBase
+    {
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        private readonly ILogger<EfficacyController> Logger;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public EfficacyController(ILogger<EfficacyController> logger)
+        {
+            Logger = logger;
+        }
+
+        /// <summary>
+        /// Returns the efficacy of the Pokemon with the given species name.
+        /// </summary>
+        [HttpGet("{name}")]
+        public async Task<double[]> GetEfficacyByName(string name)
+        {
+            Logger.LogInformation($"Getting efficacy for Pokemon \"{name}\"...");
+            var data = await PokeApiData.GetInstance();
+            var pokemon = await data.Get<Pokemon>(name);
+            return await pokemon.GetTypeEfficacyArr();
+        }
+    }
+}
