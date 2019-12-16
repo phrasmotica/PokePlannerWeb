d847bff93de95bc8e94c314792c4e89b865abab8
Julian Lawrance <julian.lawrance@gmail.com>
2019-12-16 21:00:14 +0000

    Create methods to return a Pokemon's types in a given version group or generation

--- PokePlannerWeb.Data/Extensions/PokemonExtensions.cs
+++ PokePlannerWeb.Data/Extensions/PokemonExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using PokeApiNet.Models;
+using PokePlannerWeb.Data.Mechanics;
 using PokePlannerWeb.Data.Payloads;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Type = PokePlannerWeb.Data.Mechanics.Type;
 
@@ -17,6 +19,63 @@ namespace PokePlannerWeb.Data.Extensions
         public static IEnumerable<Type> GetCurrentTypes(this Pokemon pokemon)
         {
             return pokemon.Types.ToTypes();
+        }
+
+        /// <summary>
+        /// Returns this Pokemon's types in the current version group.
+        /// </summary>
+        public static async Task<IEnumerable<Type>> GetTypes(this Pokemon pokemon)
+        {
+            var data = await PokeApiData.GetInstance();
+            var versionGroup = data.VersionGroup;
+            var types = await pokemon.GetTypes(versionGroup);
+            return types;
+        }
+
+        /// <summary>
+        /// Returns this Pokemon's types in the given version group.
+        /// </summary>
+        private static async Task<IEnumerable<Type>> GetTypes(this Pokemon pokemon, VersionGroup versionGroup)
+        {
+            var data = await PokeApiData.GetInstance();
+            var generation = await data.Get(versionGroup.Generation);
+            var pastTypes = await pokemon.GetPastTypes(generation);
+            return pastTypes.Any() ? pastTypes : pokemon.GetCurrentTypes();
+        }
+
+        /// <summary>
+        /// Returns this Pokemon's type data for the given generation, if any.
+        /// </summary>
+        private static async Task<IEnumerable<Type>> GetPastTypes(this Pokemon pokemon, Generation generation)
+        {
+            var data = await PokeApiData.GetInstance();
+            var pastTypes = pokemon.PastTypes;
+            var pastTypeGenerations = await data.Get(pastTypes.Select(t => t.Generation));
+
+            if (pastTypeGenerations.Any())
+            {
+                // use the earliest generation after the given one with past type data, if it exists
+                var laterGens = pastTypeGenerations.Where(g => g.Id >= generation.Id).ToList();
+                if (laterGens.Any())
+                {
+                    var genToUse = laterGens.Aggregate((g, h) => g.Id < h.Id ? g : h);
+                    return pastTypes.Single(p => p.Generation.Name == genToUse.Name)
+                                    .Types
+                                    .ToTypes();
+                }
+            }
+
+            return Enumerable.Empty<Type>();
+        }
+
+        /// <summary>
+        /// Returns this Pokemon's type efficacy as an array.
+        /// </summary>
+        public static async Task<double[]> GetTypeEfficacyArr(this Pokemon pokemon)
+        {
+            var types = await pokemon.GetTypes();
+            var typeData = await TypeData.GetInstance();
+            return typeData.GetEfficacyArr(types);
         }
 
         /// <summary>
