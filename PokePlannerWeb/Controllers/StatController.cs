cd95d6bc752dd8c3f038723c39f6705272d05290
Julian Lawrance <julian.lawrance@gmail.com>
2019-12-23 19:03:27 +0000

    Create controller with endpoint for fetching names of base stats in a version group

---
+++ PokePlannerWeb/Controllers/StatController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PokeApiNet.Models;
+using PokePlannerWeb.Data.Mechanics;
+
+namespace PokePlannerWeb.Controllers
+{
+    /// <summary>
+    /// Controller for getting stats.
+    /// </summary>
+    public class StatController : ResourceController<Stat>
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public StatController(ILogger<ResourceController<Stat>> logger) : base(logger)
+        {
+        }
+
+        /// <summary>
+        /// Loads the stat data from PokeAPI.
+        /// </summary>
+        protected override Task LoadResources()
+        {
+            return StatData.Instance.LoadData();
+        }
+
+        /// <summary>
+        /// Returns the names of the base stats in the version group with the given ID.
+        /// </summary>
+        [HttpGet("baseStatNames/{versionGroupId:int}")]
+        public string[] GetBaseStatNamesInVersionGroup(int versionGroupId)
+        {
+            Logger.LogInformation($"Getting names of base stats in version group {versionGroupId}...");
+            return StatData.Instance.GetBaseStatNames(versionGroupId).ToArray();
+        }
+    }
+}
