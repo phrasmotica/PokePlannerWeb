54c69742714e7e9667e01e68f2498e929f18650f
Julian Lawrance <julian.lawrance@gmail.com>
2020-01-18 12:26:03 +0000

    Create base class for database entries

---
+++ PokePlannerWeb.Data/DataStore/Models/EntryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PokePlannerWeb.Data.DataStore.Models
+{
+    /// <summary>
+    /// Base class for database entries.
+    /// </summary>
+    public class EntryBase
+    {
+        /// <summary>
+        /// Gets or sets the ID of the entry.
+        /// </summary>
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        /// <summary>
+        /// Gets the time that the entry was created.
+        /// </summary>
+        public DateTime Timestamp => Id.CreationTime;
+    }
+}
--- PokePlannerWeb.Data/DataStore/Models/PokemonEntry.cs
+++ PokePlannerWeb.Data/DataStore/Models/PokemonEntry.cs
@@ -1,27 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 
 namespace PokePlannerWeb.Data.DataStore.Models
 {
     /// <summary>
     /// Represents a Pokemon in the data store.
     /// </summary>
-    public class PokemonEntry
+    public class PokemonEntry : EntryBase
     {
-        /// <summary>
-        /// Gets or sets the ID of the entry.
-        /// </summary>
-        [BsonId]
-        public ObjectId Id { get; set; }
-
-        /// <summary>
-        /// Gets the time that the entry was created.
-        /// </summary>
-        public DateTime Timestamp => Id.CreationTime;
-
         /// <summary>
         /// Gets or sets the ID of the Pokemon.
         /// </summary>
