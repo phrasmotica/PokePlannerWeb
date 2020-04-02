e7226822b921cc58e2028ec7006f0297f38bea1f
Julian Lawrance <julian.lawrance@gmail.com>
2020-04-02 13:07:30 +0100

    Remove Mongo DB references from data store services

--- PokePlannerWeb.Data/DataStore/Services/GenerationsService.cs
+++ PokePlannerWeb.Data/DataStore/Services/GenerationsService.cs
@@ -1,7 +1,6 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using MongoDB.Driver;
 using PokeApiNet;
 using PokePlannerWeb.Data.DataStore.Abstractions;
 using PokePlannerWeb.Data.DataStore.Models;
--- PokePlannerWeb.Data/DataStore/Services/NamedApiResourceServiceBase.cs
+++ PokePlannerWeb.Data/DataStore/Services/NamedApiResourceServiceBase.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using MongoDB.Driver;
 using PokeApiNet;
 using PokePlannerWeb.Data.DataStore.Abstractions;
 using PokePlannerWeb.Data.DataStore.Models;
