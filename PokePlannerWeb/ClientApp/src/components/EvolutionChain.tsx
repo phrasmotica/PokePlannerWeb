58fe8d0cdd189d111e8b3bf0c892c5e77aa1a974
Julian Lawrance <julian.lawrance@gmail.com>
2020-04-11 18:35:25 +0100

    Render evolution details between chain members

--- PokePlannerWeb/ClientApp/src/components/EvolutionChain.tsx
+++ PokePlannerWeb/ClientApp/src/components/EvolutionChain.tsx
@@ -3,7 +3,7 @@ import { Button } from "reactstrap"
 
 import { IHasIndex } from "./CommonMembers"
 
-import { EvolutionChainEntry, ChainLinkEntry } from "../models/EvolutionChainEntry"
+import { EvolutionChainEntry, ChainLinkEntry, EvolutionDetailEntry } from "../models/EvolutionChainEntry"
 
 import "./EvolutionChain.scss"
 import key from "weak-key"
@@ -116,13 +116,8 @@ export class EvolutionChain extends Component<IEvolutionChainProps, IEvolutionCh
         let chainLinks = [this.renderChainLink(chain)]
 
         while (chain.evolvesTo.length > 0) {
-            chainLinks.push(
-                <span key={key(chain.species)}>
-                    ->
-                </span>
-            )
-
             for (let c of chain.evolvesTo) {
+                chainLinks.push(this.renderTransition(c))
                 chainLinks.push(this.renderChainLink(c))
             }
 
@@ -156,9 +151,147 @@ export class EvolutionChain extends Component<IEvolutionChainProps, IEvolutionCh
             <Button
                 key={key(link)}
                 color="link"
+                className="chain-member"
                 onMouseUp={() => this.props.setSpecies(speciesId)}>
                 {nameElement}
             </Button>
+        )
+    }
+
+    /**
+     * Renders the chain link.
+     */
+    renderTransition(link: ChainLinkEntry) {
+        let details = link.evolutionDetails.map(d => this.renderEvolutionDetail(d))
+
+        return (
+            <div key={key(link.species)}>
+                {details}
+            </div>
+        )
+    }
+
+    /**
+     * Renders the evolution detail.
+     */
+    renderEvolutionDetail(detail: EvolutionDetailEntry) {
+        let items = []
+
+        let trigger = detail.trigger
+        if (trigger !== null) {
+            let triggerElement = undefined
+
+            // only need extra info for certain triggers
+            // use-item is clear from other details
+            if (trigger.id === 1 && detail.minLevel === null) {
+                triggerElement = "level up"
+            }
+            else if (trigger.id === 2) {
+                triggerElement = "trade"
+            }
+            else if (trigger.id === 4) {
+                triggerElement = "shed"
+            }
+
+            if (triggerElement !== undefined) {
+                items.push(<span>{triggerElement}</span>)
+            }
+        }
+
+        if (detail.item !== null) {
+            items.push(<span>use {detail.item.name}</span>)
+        }
+
+        let gender = detail.gender
+        if (gender !== null) {
+            // TODO: add gender entry to EvolutionDetailEntry (but no localised names yet...)
+            let genderName = "female"
+            if (gender === 2) {
+                genderName = "male"
+            }
+            else if (gender === 3) {
+                genderName = "genderless"
+            }
+
+            items.push(<span>must be {genderName}</span>)
+        }
+
+        if (detail.heldItem !== null) {
+            items.push(<span>holding a {detail.heldItem.name}</span>)
+        }
+
+        if (detail.knownMove !== null) {
+            items.push(<span>knowing {detail.knownMove.name}</span>)
+        }
+
+        if (detail.knownMoveType !== null) {
+            items.push(<span>knowing a {detail.knownMoveType.name}-type move</span>)
+        }
+
+        if (detail.location !== null) {
+            items.push(<span>at {detail.location.name}</span>)
+        }
+
+        if (detail.minLevel !== null) {
+            items.push(<span>level {detail.minLevel}</span>)
+        }
+
+        if (detail.minHappiness !== null) {
+            items.push(<span>at least {detail.minHappiness} happiness</span>)
+        }
+
+        if (detail.minBeauty !== null) {
+            items.push(<span>at least {detail.minBeauty} beauty</span>)
+        }
+
+        if (detail.minAffection !== null) {
+            items.push(<span>at least {detail.minAffection} affection</span>)
+        }
+
+        if (detail.needsOverworldRain) {
+            items.push(<span>while raining</span>)
+        }
+
+        if (detail.partySpecies !== null) {
+            items.push(<span>with {detail.partySpecies.name} in the party</span>)
+        }
+
+        if (detail.partyType !== null) {
+            items.push(<span>with a {detail.partyType.name}-type in the party</span>)
+        }
+
+        let relativePhysicalStats = detail.relativePhysicalStats
+        if (relativePhysicalStats !== null) {
+            let relationElement = "Attack = Defense"
+            if (relativePhysicalStats === -1) {
+                relationElement = "Attack < Defense"
+            }
+            else if (relativePhysicalStats === 1) {
+                relationElement = "Attack > Defense"
+            }
+
+            items.push(<span>with {relationElement}</span>)
+        }
+
+        if (detail.timeOfDay !== null) {
+            items.push(<span>at {detail.timeOfDay}</span>)
+        }
+
+        if (detail.tradeSpecies !== null) {
+            items.push(<span>trade with {detail.tradeSpecies.name}</span>)
+        }
+
+        if (detail.turnUpsideDown) {
+            items.push(<span>while the 3DS is turned upside down</span>)
+        }
+
+        return (
+            <div className="transition">
+                <span style={{ fontSize: 24 }}>
+                    &#x27F6;
+                </span>
+                {items}
+            </div>
         )
     }
 
